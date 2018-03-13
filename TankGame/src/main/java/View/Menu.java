3af61e68edd5e0901ce04e88e418a2f0161caed3
Mester Károly <mester.karcsi1001@gmail.com>
2018-03-13 19:37:28 +0100

    A menü készen van.

---
+++ TankGame/src/main/java/View/Menu.java
@@ -0,0 +1,106 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Menu {
+    public static void main(String args[]){
+        inic();
+    }
+    private static void inic(){
+        final JFrame form= new JFrame();
+        form.setTitle("TankGame");
+        form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        form.setLayout(new BorderLayout());
+        form.setSize(800,600);
+
+
+        Container egesz=form.getContentPane();
+        egesz.setLayout(new GridLayout(1,1));
+        final JPanel menu=new JPanel();
+        final JPanel game=new JPanel();
+        menu.setLayout(new GridLayout(3,1));
+
+        final JButton play=new JButton();
+        play.setSize(1000,500);
+        play.setBackground(Color.GRAY);
+        play.setText("Play");
+        play.setBorder(null);
+        play.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                menu.setVisible(false);
+                game.setVisible(true);
+            }
+        });
+        play.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                play.setBackground(Color.GREEN);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                play.setBackground(Color.GRAY);
+            }
+        });
+        menu.add(play);
+
+        final JButton valami=new JButton();
+        valami.setSize(1000,500);
+        valami.setBackground(Color.GRAY);
+        valami.setText("Valami");
+        valami.setBorder(null);
+        valami.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Megnyomtad a gombot" , "Information", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        valami.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                valami.setBackground(Color.GREEN);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                valami.setBackground(Color.GRAY);
+            }
+        });
+        menu.add(valami);
+
+        final JButton exit=new JButton();
+        exit.setSize(1000,500);
+        exit.setBackground(Color.GRAY);
+        exit.setText("Exit");
+        exit.setBorder(null);
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        exit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                exit.setBackground(Color.GREEN);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                exit.setBackground(Color.GRAY);
+            }
+        });
+        menu.add(exit);
+        egesz.add(menu);
+        egesz.add(game);
+        //form.pack();
+        form.setVisible(true);
+    }
+}
