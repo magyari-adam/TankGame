5f147d388e2c20690705497bd169d5707663ac1d
Mester Károly <mester.karcsi1001@gmail.com>
2018-04-19 13:29:23 +0200

    optimalizáció

--- TankGame/src/main/java/View/Render.java
+++ TankGame/src/main/java/View/Render.java
@@ -1,9 +1,8 @@
 package View;
 
-import Engine.Bullet;
 import Engine.Engine;
-import Engine.Tank;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -11,80 +10,58 @@ import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Render extends JPanel implements KeyListener {
 
-    private BufferedImage image;
-    private int destpozX1;
-    private int destpozY1;
-    private int destpozX2;
-    private int destpozY2;
-    private int subsourcepozX1;
-    private int subsourcepozY1;
-    private int subsourcepozX2;
-    private int subsourcepozY2;
-    private Tank[] tanks;
-    private Bullet[] bullets;
+    private BufferedImage background;
+    private BufferedImage tank;
     private Engine engine;
     boolean map[][];
 
     public Render(){
+        this.addKeyListener(this);
         this.setPreferredSize(new Dimension(800,600));
         this.setMinimumSize(new Dimension(800,600));
         this.setMaximumSize(new Dimension(800,600));
-        setOpaque(false);
-        engine=new Engine(tanks,bullets);
-        map=engine.getMap().getMapRepresentation();
+
+        try {
+            background = ImageIO.read(getClass().getResource("/assets/bg.png"));
+            tank=ImageIO.read(getClass().getResource("/assets/tankfull.png"));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        //setOpaque(false);
+        engine=new Engine();
     }
 
     public void paintbattleground() {
+        map=engine.getMap().getMapRepresentation();
+        Polygon p=new Polygon();
         for (int x=0;x<800;x++){
-            for (int y=0;y<600;y++){
-                if (map[x][y]){
-                    this.getGraphics().setColor(Color.GREEN);
-                    this.getGraphics().drawLine(x,y,x,y);
-                }
+            int y=0;
+            while(y<600&&!map[x][y]){
+                    y++;
             }
+            p.addPoint(x,y);
         }
+        p.addPoint(getWidth(),getHeight());
+        p.addPoint(0,getHeight());
+        this.getGraphics().fillPolygon(p);
     }
 
-    public void paintbackgroundtopanel(BufferedImage background,int pozX, int pozY){
-            this.image=background;
-            this.destpozX1=pozX;
-            this.destpozY1=pozY;
-            this.destpozX2=this.getWidth();
-            this.destpozY2=this.getHeight();
-            this.subsourcepozX1=0;
-            this.subsourcepozY1=0;
-            this.subsourcepozX2=image.getWidth();
-            this.subsourcepozY2=image.getHeight();
-            paintComponent(this.getGraphics());
-
-
+    public void paintbackgroundtopanel(){
+        this.getGraphics().drawImage(background,0,0,this.getWidth(),this.getHeight(),0,0,background.getWidth(),background.getHeight(),null);
     }
 
-    public void paintimagetopanel(BufferedImage image,int sx1,int sy1,int sx2,int sy2,int pozX1, int pozY1,int pozX2,int pozY2){
-        this.image=image;
-        this.destpozX1=pozX1;
-        this.destpozY1=pozY1;
-        this.destpozX2=pozX2;
-        this.destpozY2=pozY2;
-        this.subsourcepozX1=sx1;
-        this.subsourcepozY1=sy1;
-        this.subsourcepozX2=sx2;
-        this.subsourcepozY2=sy2;
-        paintComponent(this.getGraphics());
+    public void paintimagetopanel(int pozX1, int pozY1){
+        int height=tank.getHeight();
+        int width=tank.getWidth();
+        this.getGraphics().drawImage(tank,pozX1,pozY1,pozX1+width,pozY1+height,0,0,width,height,null);
         //https://stackoverflow.com/questions/24063351/drawing-certain-parts-of-image-offset-from-the-corner
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image,destpozX1,destpozY1,destpozX2,destpozY2,subsourcepozX1,subsourcepozY1,subsourcepozX2,subsourcepozY2,null);
-    }
-    public void refresh(){
-
-    }
     public BufferedImage rotation(BufferedImage image,double theta){
         AffineTransform tx = new AffineTransform();
         tx.rotate(theta, image.getWidth() / 2, image.getHeight() / 2);
@@ -92,6 +69,11 @@ public class Render extends JPanel implements KeyListener {
         AffineTransformOp op = new AffineTransformOp(tx,
                 AffineTransformOp.TYPE_BILINEAR);
         return op.filter(image, null);
+    }
+    public void  refresh(){
+        paintbackgroundtopanel();
+        paintbattleground();
+        paintimagetopanel(50,50);
     }
 
 
@@ -129,5 +111,10 @@ public class Render extends JPanel implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    public void start() {
+        refresh();
+        //Timer t=new Timer();
     }
 }
