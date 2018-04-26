4541a82498cb9d085ae3853f9436e1cca61e0054
grimmdaniel <grimmdani@inf.elte.hu>
2018-04-26 22:17:10 +0200

    collision detection + tick

--- TankGame/src/main/java/Engine/Engine.java
+++ TankGame/src/main/java/Engine/Engine.java
@@ -11,12 +11,13 @@ public class Engine {
 
 
     private final transient int VEC_LENGTH = 2;
+    private final transient int COLLISION_RADIUS = 20;
 
     public Engine() {
         this.mapModel = new MapModel();
         tanks = new ArrayList<>();//nope
         tanks.add(new Tank(new Vec2D(50, mapModel.getVerticalPosition(50)-60), 10));//nope
-        tanks.add(new Tank(new Vec2D(300, 50), 10));//nope
+        tanks.add(new Tank(new Vec2D(300, mapModel.getVerticalPosition(300)-60), 10));//nope
         bullets = new ArrayList<>();
         bullets.add( new Bullet(new Vec2D(145, 55), new Vec2D()));
     }
@@ -54,12 +55,12 @@ public class Engine {
 
 
     public void move(int newPlace){
-        this.tanks.get(0).setPosition(new Vec2D(newPlace,mapModel.getVerticalPosition(newPlace)-60));
+        this.tanks.get(0).setPosition(new Vec2D(newPlace,mapModel.getVerticalPosition(newPlace)-30));
     }
 
     public void move(int newPlace, int tankID){
         if (tankID >= tanks.size() || tankID < 1){ return; }
-        this.tanks.get(tankID).setPosition(new Vec2D(newPlace,mapModel.getVerticalPosition(newPlace)-60));
+        this.tanks.get(tankID).setPosition(new Vec2D(newPlace,mapModel.getVerticalPosition(newPlace)-30));
     }
 
     public void shoot(int facing, int tankID){
@@ -70,6 +71,38 @@ public class Engine {
         bullets.add(new Bullet(new Vec2D(tank.getPosition().getX() * facing,tank.getPosition().getY()),new Vec2D((int)Math.round(x) * facing,(int)Math.round(y))));
     }
 
+
+    public void tick(){
+        for (Bullet x : bullets){
+            Vec2D newVec = x.getPosition();
+            newVec.add(x.getVelocity());
+            x.setPosition(new Vec2D(newVec));
+            x.setVelocity(new Vec2D(0,1));
+            for (Tank tank : tanks){
+                if (detectCollision(tank.getPosition(),x.getPosition())){
+                    // talalat erte a tankot
+                    tank.setHealth(tank.getHealth() - 1);
+                    if (tank.getHealth() == 0){
+                        //tank felrobbant
+                    }
+                }
+            }
+        }
+
+    }
+
+    public boolean detectCollision(Vec2D tank, Vec2D bullet){
+        if (COLLISION_RADIUS >= getDistanceBetween(tank,bullet)){
+            return true;
+        }
+        return false;
+    }
+
+    private int getDistanceBetween(Vec2D lhs,Vec2D rhs){
+        int horizontalDistance = Math.abs(lhs.getX() - rhs.getX());
+        int verticalDistance = Math.abs(lhs.getY() - rhs.getY());
+        return (int)Math.sqrt(horizontalDistance + verticalDistance);
+    }
 
     public MapModel getMap() {
         return this.mapModel;
