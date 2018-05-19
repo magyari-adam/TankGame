52aea6bcea64f2ab29ec5158274c7155c1ddac85
grimmdaniel <grimmdani@inf.elte.hu>
2018-05-19 14:28:37 +0200

    Tank comments added

--- TankGame/src/main/java/Engine/Tank.java
+++ TankGame/src/main/java/Engine/Tank.java
@@ -3,6 +3,10 @@ package Engine;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ *
+ * This class represents the Tank type
+ */
 public class Tank implements Serializable{
 
     private static final long serialVersionUID = 70L;
@@ -12,6 +16,10 @@ public class Tank implements Serializable{
     private int turretAngle;
     private int angleToTerrain;
 
+    /**
+     *
+     * Tank ctor without params: creates a Tank object with default values.
+     */
     public Tank(){
         position = new Vec2D();
         health = 0;
@@ -19,6 +27,11 @@ public class Tank implements Serializable{
         angleToTerrain = 0;
     }
 
+    /**
+     *
+     * @param position defines the starting position of the tank
+     * @param health initial health of the tank
+     */
     public Tank(Vec2D position, int health){
         this.position = position;
         this.health = health;
@@ -26,6 +39,14 @@ public class Tank implements Serializable{
         this.angleToTerrain = 0;
     }
 
+
+    /**
+     *
+     * @param position defines the starting position of the tank
+     * @param health initial health of the tank
+     * @param turretAngle defines the angle of the turret, should be between -20 and 50
+     * @param angleToTerrain defines the tank's angle
+     */
     public Tank(Vec2D position, int health, int turretAngle, int angleToTerrain){
         this.position = position;
         this.health = health;
@@ -33,26 +54,52 @@ public class Tank implements Serializable{
         this.angleToTerrain = angleToTerrain;
     }
 
+
+    /**
+     *
+     * @return returns the position of the tank
+     */
     public Vec2D getPosition() {
         return new Vec2D(position);
     }
 
+    /**
+     *
+     * @param position sets the position of the tank
+     */
     public void setPosition(Vec2D position) {
         this.position = position;
     }
 
+
+    /**
+     *
+     * @return returns the health of the tank
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     *
+     * @param health sets the health of the tank
+     */
     public void setHealth(int health) {
         this.health = health;
     }
 
+    /**
+     *
+     * @return returns the angle of the turret
+     */
     public int getTurretAngle() {
         return turretAngle;
     }
 
+    /**
+     *
+     * @param turretAngle set the turretangle to the given value. The parameter should be between -20 and 50.
+     */
     public void setTurretAngle(int turretAngle) {
         if (turretAngle > 50){
             this.turretAngle = 50;
@@ -63,14 +110,27 @@ public class Tank implements Serializable{
         }
     }
 
+    /**
+     *
+     * @return returns the angleToTerrain
+     */
     public int getAngleToTerrain() {
         return angleToTerrain;
     }
 
+    /**
+     *
+     * @param angleToTerrain sets the angle to the given value
+     */
     public void setAngleToTerrain(int angleToTerrain) {
         this.angleToTerrain = angleToTerrain;
     }
 
+    /**
+     *
+     * @param o the second tank, should be type of Tank
+     * @return returns true if two Tanks are equal in all of their parameters, else returns false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,11 +142,19 @@ public class Tank implements Serializable{
                 Objects.equals(position, tank.position);
     }
 
+    /**
+     *
+     * @return generates hash value for the tank
+     */
     @Override
     public int hashCode() {
         return Objects.hash(position, health, turretAngle, angleToTerrain);
     }
 
+    /**
+     *
+     * @return Textual representation of the actual Tank object
+     */
     @Override
     public String toString() {
         return "Tank{" +
